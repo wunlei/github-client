7df920beb1e7c7411b4fe6f3dbe5f6e38cde3d0e
wunlei <confusioncosmos@gmail.com>
2025-04-20 22:18:41 +0300

    Add components: PageHeader

--- src/App/pages/UserPage/UserPage.tsx
+++ src/App/pages/UserPage/UserPage.tsx
@@ -1,15 +1,12 @@
 import { observer } from 'mobx-react-lite';
 import * as React from 'react';
-import { useCallback } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router';
+import { useParams } from 'react-router';
 import Avatar from 'components/Avatar';
-import Button from 'components/Button';
 import ErrorMsg from 'components/ErrorMsg';
 import Loader from 'components/Loader';
+import PageHeader from 'components/PageHeader';
 import PageLayout from 'components/PageLayout';
 import Typography from 'components/Typography';
-import ArrowDownIcon from 'components/icons/ArrowDownIcon';
-import { routes } from 'config/router';
 import Repo from 'pages/UserPage/components/Repo/Repo';
 import UserStats from 'pages/UserPage/components/UserStats/UserStats';
 import { useInitUserPage } from 'pages/UserPage/hooks';
@@ -17,21 +14,11 @@ import { useUserPageStore } from 'store/UserPageStore';
 import s from './UserPage.module.scss';
 
 const UserPage: React.FC = observer(() => {
-  const navigate = useNavigate();
-  const routerLocation = useLocation();
   const { username } = useParams();
 
   const store = useUserPageStore();
   const { data } = store;
   const { isLoading, isError } = store.metaStore;
-
-  const handleNavigateBack = useCallback(() => {
-    if (routerLocation.key !== 'default') {
-      navigate(-1);
-    } else {
-      navigate(routes.home);
-    }
-  }, [routerLocation.key, navigate]);
 
   useInitUserPage();
 
@@ -66,18 +53,13 @@ const UserPage: React.FC = observer(() => {
   return (
     <PageLayout>
       <div className={s.content}>
-        <div className={s.header}>
-          <Button size="icon" variant="ghost" onClick={handleNavigateBack}>
-            <ArrowDownIcon width={32} height={32} color="accent" className={s.arrowBack} />
-          </Button>
+        <PageHeader>
           <Avatar src={avatarUrl} alt={login} />
           <div className={s.headerName}>
-            <Typography tag="h1" weight="bold">
-              {name}
-            </Typography>
+            <Typography view="title">{name}</Typography>
             <Typography view="p-20">{login}</Typography>
           </div>
-        </div>
+        </PageHeader>
         <div className={s.main}>
           <UserStats
             followers={followers}
