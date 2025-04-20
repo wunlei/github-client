f0c62753595e534ae7f5c6a14fd6d8f3d4a19bf9
wunlei <confusioncosmos@gmail.com>
2025-04-20 23:24:41 +0300

    Add UserSearchPage: visited users, reset data on location reset

--- src/App/pages/UserSearchPage/UserSearchPage.tsx
+++ src/App/pages/UserSearchPage/UserSearchPage.tsx
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import * as React from 'react';
-import { useCallback } from 'react';
-import { useSearchParams } from 'react-router';
+import { useCallback, useEffect } from 'react';
+import { useLocation, useSearchParams } from 'react-router';
 import { useInitUserSearchPage } from 'App/pages/UserSearchPage/hooks';
 import ErrorMsg from 'components/ErrorMsg';
 import Loader from 'components/Loader';
@@ -9,16 +9,20 @@ import PageLayout from 'components/PageLayout';
 import Typography from 'components/Typography';
 import { useUserSearchPageStore } from 'store/UserSearchPageStore';
 import List from './components/List';
-import ReposPagination from './components/ReposPagination';
-import ReposSearch from './components/ReposSearch';
+import UsersPagination from './components/UsersPagination';
+import UsersSearch from './components/UsersSearch';
 import s from './UserSearchPage.module.scss';
 
 const UserSearchPage: React.FC = observer(() => {
+  const location = useLocation();
+
   const [searchParams, setSearchParams] = useSearchParams();
   const store = useUserSearchPageStore();
 
+  const { reset } = store;
   const { currPageItems } = store.paginationStore;
   const { isLoading, isError, isSuccess, isInitial, errorMessage } = store.metaStore;
+  const { data: visitedUsers } = store.visitedUsersStore;
 
   const handlePageChange = useCallback(
     (n: number) => {
@@ -40,8 +44,15 @@ const UserSearchPage: React.FC = observer(() => {
     [searchParams, setSearchParams],
   );
 
+  useEffect(() => {
+    if (location.search == '' && location.key !== 'default') {
+      reset();
+    }
+  }, [reset, location.search, location.key]);
+
   useInitUserSearchPage();
 
+  const showVisited = isInitial && visitedUsers.length > 0;
   const showList = isSuccess && !isInitial;
 
   return (
@@ -51,14 +62,22 @@ const UserSearchPage: React.FC = observer(() => {
           List of users
         </Typography>
         <div className={s.filters}>
-          <ReposSearch onChange={handleUserChange} />
+          <UsersSearch onChange={handleUserChange} />
         </div>
         {isLoading && <Loader className={s.loader} />}
         {isError && <ErrorMsg message={errorMessage || ''} />}
+        {showVisited && (
+          <>
+            <Typography weight="bold" view="p-20" className={s.subtitle}>
+              Recently viewed users:
+            </Typography>
+            <List users={visitedUsers} />
+          </>
+        )}
         {showList && (
           <>
             <List users={currPageItems} />
-            <ReposPagination onChange={handlePageChange} className={s.pagination} />
+            <UsersPagination onChange={handlePageChange} className={s.pagination} />
           </>
         )}
       </div>
